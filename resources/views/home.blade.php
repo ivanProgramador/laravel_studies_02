9de0ba8d31555f6cf847fc70e289dd71a6308062
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-07-19 22:32:43 -0300

    configurando modo produção e desenvolvimento

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,33 +2,7 @@
 @section('content')
 
 
-{{-- Exibindo php puro ---}}
 
-@php
-  $valor=100;
-
-  // essa variavel vai mostrar um componente usando uma classe do bootstrap
-  // fazendo dessa forma mesmo com php puro eu consigo renderizar tables,buttons e etc 
-  $valor1 = '<span class="text text-warning">'.$valor."<span>";
-  
-  $nome ="Ivan Rodrigues"; 
-
-
-@endphp
-
- <h2>{{ $valor }}</h2>
-
- {{-- nesse caso estou mostando um php puro que traz um componete bootstrap  --}}
- <h2>{!! $valor1 !! }</h2>
-
-
- <h2> {{  $valor * 1000 }}</h2>
-
- //mostrando o reultado de uma função nativa do php puro no blade
-
- <h3> $nome tem <span class="text-info"> {{ strlen($nome)}} </span> carcteres </h3>
-  
-  
 @endsection
 
   
